0dda48fc7430b7c2c4358d4d04a7e8a2b3be2ba3
EC2 Default User <ec2-user@ip-10-1-1-29.us-west-2.compute.internal>
2022-08-31 01:16:41 +0000

    updated

--- query/pod/kubernetes_deployment_pod_replicas.sql
+++ query/pod/kubernetes_deployment_pod_replicas.sql
@@ -2,14 +2,15 @@ select
   -- Required Columns
   uid as resource,
   case 
-    when replicas > '3' then 'ok' 
+    when replicas >= '3' then 'ok' 
     else 'alarm' 
   end as status,
   case
-    when replicas > '3' then  name || ' has more than 3 replicas. Add atleast 3 replicas for high availability <a href="google.com">google</a>'
-    else name || ' has less than 3 replicas'
+    when replicas >='3' then  name || ' has 3 or more replicas. '
+    else name || ' has less than 3 replicas. Add atleast 3 replicas for high availability'
   end as reason,
   -- Additional Dimensionss
-  namespace || ' (namespace)'
+  context_name,
+  namespace
 from
   kubernetes_deployment;
