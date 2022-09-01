e3c7c2807cabd09882acd824de3837c00f4e5209
EC2 Default User <ec2-user@ip-10-1-1-29.us-west-2.compute.internal>
2022-09-01 00:46:26 +0000

    updated

--- query/pod/pod_service_account_token_disabled.sql
+++ query/pod/pod_service_account_token_disabled.sql
@@ -6,10 +6,10 @@ select
     else 'ok'
   end as status,
   case
-    when automount_service_account_token then 'Pod service account token will be automatically mounted.'
-    else 'Pod service account token will not be automatically mounted.'
+    when automount_service_account_token then 'Pod service account token on '|| name ||' will be automatically mounted.'
+    else 'Pod service account token '|| name ||' will not be automatically mounted.'
   end as reason,
   -- Additional Dimensions
-  context_name
+  name || ' (pod name) on ' || context_name || ' (cluster name)'
 from
   kubernetes_pod;
